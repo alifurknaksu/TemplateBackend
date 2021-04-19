4eb4795bc574cc0074418f83195c5d680d725526
Ali Furkan Aksu <ali_furkan_aksu@hotmail.com>
2021-04-20 01:08:05 +0300

    BTK-ETicaret

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 using DataAccess.Concrete.EntityFramework;
-
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -10,6 +10,24 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            EfProductDal efProductDal = new EfProductDal();
+
+            Product product1 = new Product 
+            { 
+                CategoryId = 1, 
+                BrandId = 1, 
+                Name = "Su", 
+                CreateDate = DateTime.UtcNow, 
+                Code = "WTR01",
+                Active=true
+            };
+
+            efProductDal.Add(product1);
+
+            foreach (var product in efProductDal.GetAll(p=>p.CategoryId==1))
+            {
+                Console.WriteLine(product.Name);
+            }
             
         }
 
